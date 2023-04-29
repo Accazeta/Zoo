6737d853729a181cca1b889b205db992dc59a80a
Accazeta <f.panzeri24@studenti.unibg.it>
2023-04-29 12:54:43 +0200

    Basic structure of class Animale

---
+++ Zoo/src/Zoo.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+
+#include "Zoo.hpp"
+#include "Animale.hpp"
+using namespace std;
+
+Zoo::Zoo() {}
+
+Zoo::~Zoo() {}
+
+void Zoo::addRecinto(recinto r) {
+	this->recinti_.push_back(r);
+};
+
+void Zoo::addAnimalToRecinto(int index, unique_ptr<Animale> daAggiungere) {
+	// move sposta il contenuto del vettore "daAggiungere" all'interno del vettore "recinto_animali"
+	this->recinti_[index].recinto_animali.push_back(move(daAggiungere));
+}
+
+void Zoo::daiDaMangiare() {
+
+}
--- Zoo/src/Zoo.hpp
+++ Zoo/src/Zoo.hpp
@@ -1,8 +1,30 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+
+#include "Animale.hpp"
+using namespace std;
+
 #ifndef ZOO_HPP_
 #define ZOO_HPP_
 
+struct recinto {
+	string nome;
+	vector<unique_ptr<Animale>> recinto_animali;
+};
 
+class Zoo {
 
+public:
+	void addRecinto(recinto r);
+	void addAnimalToRecinto(int recintoIndex, unique_ptr<Animale> daAggiungere);
+	void daiDaMangiare();
+	Zoo();
+	~Zoo();
 
+private:
+	vector<recinto> recinti_;
+
+};
 
 #endif /* ZOO_HPP_ */

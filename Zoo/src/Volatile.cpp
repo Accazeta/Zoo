f0283737b32c0212476b1f33ca2d8a02a97896c2
Accazeta <f.panzeri24@studenti.unibg.it>
2023-05-01 11:29:32 +0200

    Created class Volatile

---
+++ Zoo/src/Volatile.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <random>
+#include "Animale.hpp"
+#include "Volatile.hpp"
+using namespace std;
+// Costruttore
+Volatile::Volatile(string nome, float peso, int numUova)
+	: Animale(nome, peso){
+	this->numUova = numUova;
+};
+
+// Distruttore
+Volatile::~Volatile() {};
+
+void Volatile::vola() {
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_real_distribution<> distribuzione(0,100);
+	// Estraggo un numero da 0 a 100. Se minore di 40 (40% di probabilità)
+	// allora il volatile diventa affamato, altrimenti non faccio nulla
+	float val = distribuzione(gen);
+	if(val < 40) {
+		this->affamato = true;
+	}
+};
+
+void Volatile::emettiSuono() {
+	cout<<"Suono di un Volatile\n";
+};
+
+void Volatile::mangia() {
+	Animale::affamato = false;
+};
+
---
+++ Zoo/src/Volatile.hpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Animale.hpp"
+using namespace std;
+
+#ifndef UCCELLO_HPP_
+#define UCCELLO_HPP_
+
+class Volatile: public virtual Animale {
+public:
+	Volatile(string nome, float peso, int numUova);
+	virtual ~Volatile();
+	void emettiSuono();
+	void mangia();
+
+protected:
+	int numUova;
+	void vola();
+};
+
+#endif /* UCCELLO_HPP_ */

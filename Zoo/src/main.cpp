c6cc28d27750419e9c3f50d820795019c1e2dd8b
Accazeta <f.panzeri24@studenti.unibg.it>
2023-05-02 11:30:52 +0200

    Added few test objects and code for debug/testing

--- Zoo/src/main.cpp
+++ Zoo/src/main.cpp
@@ -10,8 +10,23 @@
 #include "Pappagallo.hpp"
 using namespace std;
 
+void printZoo(Zoo input) {
+	for(auto &recint : input.getRecinti()) {
+		cout<<"Recinto " + recint.get()->nome + " contiene: "<<endl;
+		for(auto &anim : recint.get()->animali) {
+			cout<<anim->getName() + " di tipo " + typeid(anim).name() + " | ";
+		}
+		cout<<"\n";
+	}
+}
+
+
+
 int main() {
 	Zoo * mioZoo = new Zoo();
+
+	// Aggiunta recinti
+
 	shared_ptr<recinto> r1 = make_shared<recinto>();
 	r1.get()->nome = "Elefanti";
 
@@ -25,6 +40,8 @@ int main() {
 	mioZoo->addRecinto(r2);
 	mioZoo->addRecinto(r3);
 
+	// Aggiunta Elefanti
+
 	Animale * ae = new Elefante("El1", 5900.3, GRIGIO, 2.4);
 	cout<< "Elefante " + ae->getName() + " è di tipo "+ typeid(ae).name()<<endl; //debug
 	Mammifero * me = new Elefante("El2", 5850.7, GRIGIO, 2.6);
@@ -36,8 +53,31 @@ int main() {
 	mioZoo->addAnimalToRecinto("Elefanti", me);
 	mioZoo->addAnimalToRecinto("Elefanti", ee);
 
+	// Aggiunta pipistrelli
 
+	Animale * api = new Pipistrello("Pip1", 1.3, NERO, 3, 180);
+	Volatile * vpi = new Pipistrello("Pip2", 1.5, NERO, 4, 185.3);
+	Mammifero * mpi = new Pipistrello("Pip3", 1.2, NERO, 2, 195.1);
+	Pipistrello * ppi = new Pipistrello("Pip4", 1.6, NERO, 5, 191.9);
 
+	mioZoo->addAnimalToRecinto("Pipistrelli", api);
+	mioZoo->addAnimalToRecinto("Pipistrelli", vpi);
+	mioZoo->addAnimalToRecinto("Pipistrelli", mpi);
+	mioZoo->addAnimalToRecinto("Pipistrelli", ppi);
+
+	// Aggiunta Pappagalli
+
+	Animale * apa = new Pappagallo("Pap1", 2.3, 4, 0.10);
+	Volatile * vpa = new Pappagallo("Pap2", 1.9, 3, 0.90);
+	Pappagallo * ppa = new Pappagallo("Pap3", 2.1, 1, 0.5);
+
+	mioZoo->addAnimalToRecinto("Pappagalli", apa);
+	mioZoo->addAnimalToRecinto("Pappagalli", vpa);
+	mioZoo->addAnimalToRecinto("Pappagalli", ppa);
+
+	printZoo(*mioZoo);
 
 	return 0;
 }
+
+
